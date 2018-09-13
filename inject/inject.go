85da91f10979d2283ab7eadd90b2219b964f2d86
运宝马 <yunbaoma@yunbaomadeMacBook-Pro.local>
2018-09-13 10:51:28 +0800

    修改锁问题

--- inject/inject.go
+++ inject/inject.go
@@ -109,23 +109,15 @@ func New() *Injector {
 }
 
 func (inject *Injector) ServiceByName(serviceName string) interface{} {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return reflect.ValueOf(inject.Get(serviceName)).Elem().Interface()
 }
 func (inject *Injector) ServicePtrByName(serviceName string) interface{} {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return reflect.ValueOf(inject.Get(serviceName)).Interface()
 }
 func (inject *Injector) Service(ptr interface{}) interface{} {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return reflect.ValueOf(inject.Get(reflect.TypeOf(ptr).String())).Elem().Interface()
 }
 func (inject *Injector) ServiceByPrefixName(prefix string) (services []interface{}) {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	for k, v := range inject.beanMap {
 		if strings.HasPrefix(k, prefix) {
 			services = append(services, reflect.ValueOf(v).Elem().Interface())
@@ -134,23 +126,17 @@ func (inject *Injector) ServiceByPrefixName(prefix string) (services []interface
 	return services
 }
 func (inject *Injector) Services() (services []interface{}) {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	for _, v := range inject.beanMap {
 		services = append(services, reflect.ValueOf(v).Elem().Interface())
 	}
 	return services
 }
 func (inject *Injector) Apply(services ... interface{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	for _, ser := range services {
 		inject.replyOnInject(ser)
 	}
 }
 func (inject *Injector) ApplyWithName(name string, service interface{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	if reflect.TypeOf(service).Kind() == reflect.Ptr {
 		inject.Map(name, service)
 	} else if reflect.TypeOf(service).Kind() == reflect.Func {
@@ -168,22 +154,16 @@ func (inject *Injector) ApplyWithName(name string, service interface{}) {
 	}
 }
 func (inject *Injector) RegisterService(services ... interface{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	for e := range services {
 		inject.Apply(e)
 	}
 	inject.Inject()
 }
 func (inject *Injector) RegisterServiceWithName(name string, service interface{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.ApplyWithName(name, service)
 	inject.Inject()
 }
 func (inject *Injector) Inject() {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	for _, value := range inject.beanMap {
 		inject.injectWithReply(value)
 	}
@@ -234,178 +214,112 @@ func (inject *Injector) replyOnInject(service interface{}) {
 func (inject *Injector) Map(key string, value interface{}) {
 	inject.Locker.Lock()
 	defer inject.Locker.Unlock()
-	inject.Locker.Lock()
 	inject.beanMap[key] = value
-	inject.Locker.Unlock()
 }
 func (inject *Injector) MapStruct(key string, value *struct{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapBool(key string, value *bool) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapInt(key string, value *int) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapInt8(key string, value *int8) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapInt16(key string, value *int16) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapInt32(key string, value *int32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapInt64(key string, value *int64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 
 func (inject *Injector) MapUint(key string, value *uint) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapUint8(key string, value *uint8) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapUint16(key string, value *uint16) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapUint32(key string, value *uint32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapUint64(key string, value *uint64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 
 func (inject *Injector) MapFloat32(key string, value *float32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 func (inject *Injector) MapFloat64(key string, value *float64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 
 func (inject *Injector) MapString(key string, value *string) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 
 func (inject *Injector) MapByte(key string, value *byte) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, value)
 }
 
 func (inject *Injector) MapStructs(key string, values *[]struct{}) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
 func (inject *Injector) MapBools(key string, values *[]bool) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
 func (inject *Injector) MapInts(key string, values *[]int) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapInt8s(key string, values *[]int8) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapInt16s(key string, values *[]int16) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapInt32s(key string, values *[]int32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapInt64s(key string, values *[]int64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
 func (inject *Injector) MapUints(key string, values *[]uint) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapUint8s(key string, values *[]uint8) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapUint16s(key string, values *[]uint16) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapUint32s(key string, values *[]uint32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapUint64s(key string, values *[]uint64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
 func (inject *Injector) MapFloat32s(key string, values *[]float32) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapFloat64s(key string, values *[]float64) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
 func (inject *Injector) MapStrings(key string, values *[]string) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 func (inject *Injector) MapBytes(key string, values *[]byte) {
-	inject.Locker.Lock()
-	defer inject.Locker.Unlock()
 	inject.Map(key, values)
 }
 
@@ -417,173 +331,109 @@ func (inject *Injector) Get(key string) interface{} {
 }
 
 func (inject *Injector) GetStruct(key string) *struct{} {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*struct{})
 }
 
 func (inject *Injector) GetBool(key string) *bool {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*bool)
 }
 
 func (inject *Injector) GetInt(key string) *int {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*int)
 }
 func (inject *Injector) GetInt8(key string) *int8 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*int8)
 }
 func (inject *Injector) GetInt16(key string) *int16 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*int16)
 }
 func (inject *Injector) GetInt32(key string) *int32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*int32)
 }
 func (inject *Injector) GetInt64(key string) *int64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*int64)
 }
 
 func (inject *Injector) GetUint(key string) *uint {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*uint)
 }
 func (inject *Injector) GetUint8(key string) *uint8 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*uint8)
 }
 func (inject *Injector) GetUint16(key string) *uint16 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*uint16)
 }
 func (inject *Injector) GetUint32(key string) *uint32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*uint32)
 }
 func (inject *Injector) GetUint64(key string) *uint64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*uint64)
 }
 
 func (inject *Injector) GetFloat32(key string) *float32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*float32)
 }
 func (inject *Injector) GetFloat64(key string) *float64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*float64)
 }
 
 func (inject *Injector) GetString(key string) *string {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*string)
 }
 
 func (inject *Injector) GetByte(key string) *byte {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*byte)
 }
 
 func (inject *Injector) GetStructs(key string) *[]struct{} {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]struct{})
 }
 
 func (inject *Injector) GetInts(key string) *[]int {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]int)
 }
 func (inject *Injector) GetInt8s(key string) *[]int8 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]int8)
 }
 func (inject *Injector) GetInt16s(key string) *[]int16 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]int16)
 }
 func (inject *Injector) GetInt32s(key string) *[]int32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]int32)
 }
 func (inject *Injector) GetInt64s(key string) *[]int64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]int64)
 }
 
 func (inject *Injector) GetUints(key string) *[]uint {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]uint)
 }
 func (inject *Injector) GetUint8s(key string) *[]uint8 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]uint8)
 }
 func (inject *Injector) GetUint16s(key string) *[]uint16 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]uint16)
 }
 func (inject *Injector) GetUint32s(key string) *[]uint32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]uint32)
 }
 func (inject *Injector) GetUint64s(key string) *[]uint64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]uint64)
 }
 
 func (inject *Injector) GetFloat32s(key string) *[]float32 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]float32)
 }
 func (inject *Injector) GetFloat64s(key string) *[]float64 {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]float64)
 }
 
 func (inject *Injector) GetStrings(key string) *[]string {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]string)
 }
 func (inject *Injector) GetBytes(key string) *[]byte {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]byte)
 }
 func (inject *Injector) GetBools(key string) *[]bool {
-	inject.Locker.RLock()
-	defer inject.Locker.RUnlock()
 	return inject.Get(key).(*[]bool)
 }
